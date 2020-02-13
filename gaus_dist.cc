acfc5861c6353dc626fa0a8cd74b25eff65e2e2d
laxman paudel <laxmansharmapaudel@gmail.com>
2020-02-12 21:15:59 -0600

    gauss fit to the random numbers that follows the PD

--- gaus_dist.cc
+++ gaus_dist.cc
@@ -2,13 +2,18 @@
 #include<TRandom3.h>
 using namespace std;
 
-TH1F *h=new TH1F("h","the gaussian distribution",100,0,100);
+TH1F *h=new TH1F("h","The distribution of random numbers that follow the poisson distribution fitted with the Gaussian function ",100,0,100);
+TRandom3 generator(0);
 void gaus_dist(){
-  for (int i=0;i<1000;i++){
-    double y= gRandom->Gaus(50,10);
-  h->Fill(y);
+for (int i=0;i<5000;i++){
+  // generating 5000 random numbers that follow the P.D. having mean 50.0                           
+
+double  x=generator.PoissonD(50.0);
+  h->Fill(x);
   }
   h->Fit("gaus");
+  h-> SetXTitle("Random numbers from the P.D. distn having mean 50");
+  h->SetYTitle("Counts");
   h->Draw();
   }
 
